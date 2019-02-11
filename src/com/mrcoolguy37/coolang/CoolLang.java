08cbaa5b800c05b541dcf099d5e3f44b782b599a
dan <mrcoolguy371@gmail.com>
2019-02-11 15:39:18 -0800

    Initial commit

---
+++ src/com/mrcoolguy37/coolang/CoolLang.java
@@ -0,0 +1,22 @@
+package com.mrcoolguy37.coolang;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Coolang {
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: coolang [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+}
