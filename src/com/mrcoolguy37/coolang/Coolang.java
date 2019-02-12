3cb13a50d4df15aa46dafaf462bef5b65715eadb
dan <mrcoolguy371@gmail.com>
2019-02-11 17:49:45 -0800

    Added Token and TokenType classes

--- src/com/mrcoolguy37/coolang/Coolang.java
+++ src/com/mrcoolguy37/coolang/Coolang.java
@@ -9,6 +9,8 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Coolang {
+    static boolean hadError = false;
+
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: coolang [script]");
@@ -19,4 +21,45 @@ public class Coolang {
             runPrompt();
         }
     }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        // Indicate errors in exit code
+        if (hadError) System.exit(65);
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.println("> ");
+            run(reader.readLine());
+            hadError = false;
+        }
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println(
+            "[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+
+
+
 }
---
+++ src/com/mrcoolguy37/coolang/Token.java
@@ -0,0 +1,19 @@
+package com.mrcoolguy37.coolang;
+
+class Token {
+    final TokenType type;
+    final String lexeme;
+    final Object literal;
+    final int line;
+
+    Token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = Lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
