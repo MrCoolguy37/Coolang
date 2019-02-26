45ab1ff243efbbd67cac159b8abd2241e33dbbf0
dan <mrcoolguy371@gmail.com>
2019-02-25 17:32:54 -0800

    Added some shit, fixed some shit and broke some other shit

---
+++ src/com/mrcoolguy37/coolang/RuntimeError.java
@@ -0,0 +1,10 @@
+package com.mrcoolguy37.coolang;
+
+class RuntimeError extends RuntimeException {
+    final Token token;
+
+    RuntimeError(Token token, String message) {
+        super(message);
+        this.token = token;
+    }
+}
